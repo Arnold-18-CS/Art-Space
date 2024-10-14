75b6b77962175da297ba96feb7dc8f3b5f054e17
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-10-14 16:04:13 +0300

    Setting up the main composables and images needed

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,9 +16,44 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ArtSpaceTheme {
-
+                ArtSpaceApp()
             }
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun ArtworkWall(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun ArtworkDescription(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun DisplayController(modifier: Modifier = Modifier){
+
+}
+
+@Composable
+fun ArtSpaceLayout(modifier: Modifier){
+
+}
+
+@Preview(
+    showSystemUi = true,
+    showBackground = true,
+    name = "Art Space"
+)
+@Composable
+fun ArtSpaceApp() {
+    ArtSpaceTheme {
+        ArtSpaceLayout(modifier = Modifier
+            .fillMaxSize())
+    }
+}
+
 
