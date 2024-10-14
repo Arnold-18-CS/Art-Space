10b01f08e51bd0ef203245c670e2d3324ddbe2d4
Arnold Oguda <127428952+Arnold-18-CS@users.noreply.github.com>
2024-10-14 16:33:18 +0300

    Completed template for ArtworkWall

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +30,15 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ArtworkWall(modifier: Modifier = Modifier){
-
+    val imageResource = painterResource(id = R.drawable.japanese_landscape_with_samurai___wallpaper_4k_by_jmasaryk_dhkmy30)
+    Image(
+        painter = imageResource,
+        contentDescription = null,
+        modifier = Modifier
+            .size(400.dp, 300.dp)
+            .padding(
+                top = 10.dp, bottom = 20.dp, start = 30.dp, end = 30.dp
+            ))
 }
 
 @Composable
